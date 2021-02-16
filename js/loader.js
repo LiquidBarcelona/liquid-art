f503558e0ee092ecea07f8c01ee61b1d5c5a4293
Marc Bernet <mbernet@gmail.com>
2021-02-16 15:16:08 +0100

    favico

--- js/loader.js
+++ js/loader.js
@@ -6,10 +6,10 @@ $.ajax({
             console.log(item);
 
             $('#works-of-art').append(
-                `<div class="card" style="width: 32rem;">
+                `<div class="card" style="width: 40rem;">
                   
                   <div class="card-body">
-                    <h5 class="card-title"><a href="${item.workOfArtWiki}"> ${item.author} - ${item.title}</a></h5>
+                    <h5 class="card-title"><a href="${item.authorUrl}"> ${item.author}</a> - <a href="${item.workOfArtWiki}">${item.title}</a></h5>
                     <p class="card-text">${item.description} <span class="font-itallic">by ${item.sender}</span></p>
                   </div>
                   <img class="card-img-top" src="${item.workOfArtImg}" alt="${item.author}">
