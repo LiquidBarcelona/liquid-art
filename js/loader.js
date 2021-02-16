a62eadb97635c2bfcd247bf66afb65533f42910f
Marc Bernet <mbernet@gmail.com>
2021-02-16 16:19:19 +0100

    latests improvements + theme

--- js/loader.js
+++ js/loader.js
@@ -2,17 +2,18 @@ $.ajax({
     url: "js/art.json",
     dataType: "json",
     success: function(response) {
-        $.each(response, function(key, item) {
-            console.log(item);
+        $.each(response.reverse(), function(key, item) {
 
             $('#works-of-art').append(
                 `<div class="card" style="width: 40rem;">
                   
                   <div class="card-body">
                     <h5 class="card-title"><a href="${item.authorUrl}"> ${item.author}</a> - <a href="${item.workOfArtWiki}">${item.title}</a></h5>
-                    <p class="card-text">${item.description} <span class="font-itallic">by ${item.sender}</span></p>
                   </div>
                   <img class="card-img-top" src="${item.workOfArtImg}" alt="${item.author}">
+                  <div class="card-body">
+                   <p class="card-text">${item.description} <span class="badge badge-primary">${item.sender}</span></p>
+                  </div>
                 </div>`
             );
         });
