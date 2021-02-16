60741f546a21475cf2af1662258e43591cc5ba81
Marc Bernet <mbernet@gmail.com>
2021-02-16 17:49:01 +0100

    years

--- js/loader.js
+++ js/loader.js
@@ -3,16 +3,15 @@ $.ajax({
     dataType: "json",
     success: function(response) {
         $.each(response.reverse(), function(key, item) {
-
             $('#works-of-art').append(
                 `
                 <div class="row">
                     <div class="card d-flex h-100" style="width: 38rem;">
                       
                       <div class="card-body d-flex flex-column align-items-start">
-                        <h5 class="card-title"><a href="${item.authorUrl}"> ${item.author}</a> - <a href="${item.workOfArtWiki}">${item.title}</a></h5>
+                        <h5 class="card-title"><a href="${item.authorUrl}" target="_blank"> ${item.author}</a> - <a href="${item.workOfArtWiki}" target="_blank">${item.title}</a> (${item.year})</h5>
                       </div>
-                      <img class="card-img-top" src="${item.workOfArtImg}" alt="${item.author}">
+                      <img class="card-img-top" src="${item.workOfArtImg}" alt="${item.title}">
                       <div class="card-body">
                        <p class="card-text">${item.description} <span class="badge badge-primary">${item.sender}</span></p>
                       </div>
